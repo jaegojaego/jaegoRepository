7f0fa0f9c065c7a83f060cb6743cea76ffa93515
scit32park <scit32park@gmail.com>
2017-04-25 14:38:47 +0900

    록헌 코드 합병

--- jaegojaego/src/main/java/com/jaego/web/DAO/SellerDAO.java
+++ jaegojaego/src/main/java/com/jaego/web/DAO/SellerDAO.java
@@ -5,6 +5,64 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.jaego.web.VO.Goods;
+import com.jaego.web.VO.Seller;
+
+@Repository      //DAO 객체에 사용하는 어노테이션
+public class SellerDAO {
+
+   @Autowired
+   private SqlSession sqlsession;
+
+   //판매자 추가
+   public void addSeller(Seller seller) {
+      SellerMapper mapper = sqlsession.getMapper(SellerMapper.class);
+      mapper.addSeller(seller);
+   }
+   
+   //판매자 검색
+   public Seller selectOne(String id) {
+      SellerMapper mapper = sqlsession.getMapper(SellerMapper.class);
+      Seller seller = mapper.selectOne(id);
+      return seller;
+   }
+   //판매자 정보수정 권록헌
+   public int updateSeller(Seller seller){
+      SellerMapper mapper=sqlsession.getMapper(SellerMapper.class);
+      int result=0;
+      try{
+      result=mapper.updateSeller(seller);
+      }catch(Exception e){
+         e.printStackTrace();
+      }
+      return result;
+      
+   }
+   //판매자 사진 정보 권록헌
+   public Seller selectPic1(String sellerId){
+      SellerMapper mapper=sqlsession.getMapper(SellerMapper.class);
+      Seller seller=mapper.selectPic1(sellerId);
+      return seller;
+   }
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+/*
+//20170321 박진우
+package com.jaego.web.DAO;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.jaego.web.VO.Seller;
 
 @Repository		//DAO 객체에 사용하는 어노테이션
@@ -27,3 +85,4 @@ public class SellerDAO {
 	}
 	
 }
+*/
--- jaegojaego/src/main/java/com/jaego/web/DAO/SellerMapper.java
+++ jaegojaego/src/main/java/com/jaego/web/DAO/SellerMapper.java
@@ -1,3 +1,24 @@
+package com.jaego.web.DAO;
+
+import com.jaego.web.VO.Goods;
+import com.jaego.web.VO.Seller;
+
+public interface SellerMapper {
+   public void addSeller(Seller seller);
+   public Seller selectOne(String id);
+   public int updateSeller(Seller seller);//판매자정보수정 권록헌
+   public Seller selectPic1(String sellerId);//사진권록헌
+}
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/*
 //20170321 박진우
 package com.jaego.web.DAO;
 
@@ -7,5 +28,5 @@ import com.jaego.web.VO.Seller;
 public interface SellerMapper {
 	public void addSeller(Seller seller);
 	public Seller selectOne(String id);
-	
 }
+*/
